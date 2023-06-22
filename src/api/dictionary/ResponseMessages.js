5a5f81ecf42286315e3b57217188c584412d3f06
Rafi Fajar Sulaiman <rafihariyadi01@gmail.com>
2023-06-22 22:05:27 +0700

    Fixed bugs on dictionary/ResponseMessages

--- src/api/dictionary/ResponseMessages.js
+++ src/api/dictionary/ResponseMessages.js
@@ -1,18 +1,18 @@
 class ResponseMessages {
-    static #partial = "Data Has Been Partially Fetched";
-    static #full = "Data Has Been Completely Fetched";
-    static #fail = "Failed To Fetch Data";
+    static #PARTIAL = "Data Has Been Partially Fetched";
+    static #FULL = "Data Has Been Completely Fetched";
+    static #FAIL = "Failed To Fetch Data";
 
-    get PARTIAL() {
-        return this.#partial;
+    static get PARTIAL() {
+        return this.#PARTIAL;
     }
 
-    get FULL() {
-        return this.#full;
+    static get FULL() {
+        return this.#FULL;
     }
 
-    get FAIL() {
-        return this.#fail;
+    static get FAIL() {
+        return this.#FAIL;
     }
 }
 
